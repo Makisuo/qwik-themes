8400f972532b64eee2472f532fd8c44f4b50cab8
maiieul <maieul.chevalier@gmail.com>
2024-02-26 09:03:33 +0100

    fix(theme-script): allow updateDOM to handle multiple classes without flicker

--- src/test.tsx
+++ src/test.tsx
@@ -1,40 +1,68 @@
-import { component$ } from "@builder.io/qwik"
-import { useTheme } from "./lib/provider"
+import { component$ } from "@builder.io/qwik";
+import { useTheme } from "./lib/provider";
 
 export const Test = component$(() => {
-	const { theme, setTheme, themes } = useTheme()
-	// console.log(theme, themes, resolvedTheme)
-	return (
-		<div
-			style={{
-				display: "flex",
-				flexDirection: "column",
-				gap: 4,
-			}}
-		>
-			{theme?.toString()}
-			{themes?.map((theme) => (
-				<button
-					key={theme.toString()}
-					type="button"
-					onClick$={() => {
-						// console.log(theme)
-						setTheme(theme)
-					}}
-				>
-					{theme.toString()}
-				</button>
-			))}
+  const { theme, setTheme } = useTheme();
 
-			{/* <button
-				type="button"
-				onClick$={() => {
-					setTheme(theme === "dark" ? "light" : "dark")
-					// themeSig.value = themeSig.value === "dark" ? "light" : "dark"
-				}}
-			>
-				Toggle
-			</button> */}
-		</div>
-	)
-})
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 4,
+      }}
+    >
+      {theme?.toString()}
+      <button
+        type="button"
+        onClick$={() => {
+          setTheme("simple red");
+        }}
+      >
+        simple red
+      </button>
+      <button
+        type="button"
+        onClick$={() => {
+          setTheme("brutalist green");
+        }}
+      >
+        brutalist green
+      </button>
+      <button
+        type="button"
+        onClick$={() => {
+          setTheme("hand blue");
+        }}
+      >
+        hand blue
+      </button>
+
+      <button
+        type="button"
+        onClick$={() => {
+          setTheme("light");
+        }}
+      >
+        light
+      </button>
+      <button
+        type="button"
+        onClick$={() => {
+          setTheme("dark");
+        }}
+      >
+        dark
+      </button>
+
+      <button
+        type="button"
+        onClick$={() => {
+          setTheme("purple");
+        }}
+      >
+        purple
+      </button>
+    </div>
+  );
+});
